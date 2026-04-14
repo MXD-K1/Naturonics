090cf5b2c9b6eae4a5700a3d6834176ea529e120
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-04-14 05:48:07 +0300

    added player and movement logic

---
+++ src/entities/hero.js
@@ -0,0 +1,23 @@
+import { createEntity } from "./entity.js";
+
+export function createHero(k, pos) {
+    return [
+        ...createEntity(k, pos),
+        k.rect(20, 20),
+        k.color(255, 255, 255), // temp
+        // k.health(),
+        "player",
+        {},
+    ];
+}
+
+export function moveHero(k, hero) {
+    hero.onKeyDown((key) => {
+        const moveVec = k.vec2(0, 0);
+        if (key === "left" || key === "a") moveVec.x = -hero.speed;
+        if (key === "right" || key === "d") moveVec.x = hero.speed;
+        if (key === "down" || key === "s") moveVec.y = hero.speed;
+        if (key === "up" || key === "w") moveVec.y = -hero.speed;
+        hero.move(moveVec.x * k.dt(), moveVec.y * k.dt());
+    });
+} // TODO: fix the doubled speed when using two keys at once
--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
-import k from "./kaplayContext.js"
+import k from "./kaplayContext.js";
 import { SCENES } from "./utils/constants.js";
+import { createHero, moveHero } from "./entities/hero.js";
+import { colorizeBG } from "./utils/utils.js";
 
 // TODO: Rename the project title and update `index.html` <title>.
 // TODO: Decide on a fixed resolution vs responsive scaling strategy.
 // TODO: Configure gravity once platforming or physics are added.
 
-
 // TODO: Add a real asset loading pipeline (sprites, fonts, audio).
 
-for (scene of SCENES) {
-
-}
+// for (scene of SCENES) {
+// }
 
 // TODO: Add global input bindings (pause, mute, screenshot, etc.).
 // TODO: Add a global UI layer for FPS, version, or debug info.
 // k.go();
+
+colorizeBG(k, 0, 0, 0);
+const hero = k.add(createHero(k, k.vec2(8, 8)));
+moveHero(k, hero);
